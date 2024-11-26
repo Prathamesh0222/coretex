53c80ac147fbd6cfebeae67cc4fa2594990a5f88
Prathamesh Pimpalkar <91759972+Prathamesh0222@users.noreply.github.com>
2024-11-26 17:07:54 +0530

    Add MongoDB connection and define user and content schemas

---
+++ http/src/db.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/mediaserver");
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+})
+
+const contentTypes = ["images", "video", "audio", "article"];
+
+const contentSchema = new mongoose.Schema({
+    link: { type: String, required: true },
+    type: { type: String, enum: contentTypes, required: true },
+    title: { type: String, required: true },
+    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag" }],
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+});
+
+export const User = mongoose.model("User", userSchema);
+export const Content = mongoose.model("Content", contentSchema);
+
