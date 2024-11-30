0eb8a9f5fc7295664d3a77ef94167d1a9732401e
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-11-30 23:48:59 +0530

    Add CORS support and update dependencies in package.json and pnpm-lock.yaml

--- http/src/index.ts
+++ http/src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import userRouter from "./Router/userRouter";
 import contentRouter from "./Router/contentRouter";
 import { config } from "dotenv";
+import cors from "cors";
 
 config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1", userRouter);
 app.use("/api/v1", contentRouter);
@@ -13,5 +15,5 @@ app.use("/api/v1", contentRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT || "", () => {
-    console.log(`Server running at ${PORT}`);
-})
+  console.log(`Server running at ${PORT}`);
+});
