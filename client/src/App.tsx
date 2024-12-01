82a334d9b78a29a69abef5831c0440ce894d6d1a
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2024-12-01 23:55:34 +0530

    Refactor App component to integrate routing and state management with react-query and react-toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
 import { Signup } from "./pages/Signup";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import Dashboard from "./pages/dashboard";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <Signup />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Signup />} path="/signup" />
+          <Route element={<Signin />} path="/signin" />
+          <Route element={<Dashboard />} path="/dashboard" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
