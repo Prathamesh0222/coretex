f189388d0f12d02ce9b1c52a2fc148bd6d0f5446
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-12-03 21:11:41 +0530

    Add Navbar component with Brainly logo and dark styling

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { BrainlyIcon } from "../icons/BrainlyIcon";
+
+export const Navbar = () => {
+  return (
+    <div className="w-full p-6 bg-dark-400 border-b border-dark-300">
+      <h1 className="flex items-center gap-2 text-2xl text-white font-bold">
+        <BrainlyIcon /> Brainly
+      </h1>
+    </div>
+  );
+};
