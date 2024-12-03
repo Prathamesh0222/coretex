13dc2d04b40a4d775d54aa2f70ff30c68f8528e1
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-12-03 21:10:49 +0530

    Add rounded prop to Button component for customizable styling

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -8,6 +8,7 @@ interface ButtonProps {
   fullWidth?: boolean;
   fontWeight?: "sm" | "md" | "lg";
   disabled?: boolean;
+  rounded?: boolean;
 }
 
 const variantClasses = {
@@ -17,8 +18,7 @@ const variantClasses = {
 
 const disabledClasses = "opacity-50 cursor-not-allowed";
 
-const defaultStyles =
-  "flex justify-center items-center gap-2 px-4 py-2 rounded-md";
+const defaultStyles = "flex justify-center items-center gap-2";
 
 const fontWeights = {
   sm: "font-thin",
@@ -34,7 +34,9 @@ export const Button = ({
   fullWidth,
   fontWeight,
   disabled = false,
+  rounded,
 }: ButtonProps) => {
+  const isCircular = rounded && !text;
   return (
     <button
       onClick={disabled ? undefined : onClick}
@@ -44,10 +46,12 @@ export const Button = ({
         ${fullWidth ? "w-full" : ""}
         ${fontWeight ? fontWeights[fontWeight] : ""}
         ${disabled ? disabledClasses : ""}
+        ${rounded ? "rounded-full" : "rounded-md"}
+        ${isCircular ? "w-12 h-12" : "px-4 py-2"}
       `}
     >
       {startIcon}
-      {text}
+      {!isCircular && text}
     </button>
   );
 };
