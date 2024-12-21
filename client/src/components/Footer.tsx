78524d5bd56fb9c58fb70a47b60a0913de8f3d6c
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2024-12-21 23:57:24 +0530

    Add Footer and LandingNavbar components: create footer and navigation bar for the landing page

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,3 @@
+export const Footer = () => {
+  return <div className="p-2 border-t border-gray-500">hi there</div>;
+};
---
+++ client/src/components/LandingNavbar.tsx
@@ -0,0 +1,17 @@
+import { BrainlyIcon } from "../icons/BrainlyIcon";
+import { Button } from "./Button";
+
+export const LandingNavbar = () => {
+  return (
+    <div className="container mx-auto p-4 flex items-center text-white justify-between">
+      <div className="flex gap-2 items-center">
+        <BrainlyIcon />
+        <h1 className="text-2xl font-bold">Brainly</h1>
+      </div>
+      <div className="flex gap-2">
+        <Button variant="primary" text="Login" />
+        <Button variant="secondary" text="Join Now" fontColor="black" />
+      </div>
+    </div>
+  );
+};
