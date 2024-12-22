a75fbde5712a52960e2ac687881dcfac68c3db9c
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2024-12-22 23:46:22 +0530

    Enhance Footer component: update layout and add social media icons

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,3 +1,16 @@
+import { GithubIcon } from "../icons/GithubIcon";
+import { LinkedinIcon } from "../icons/LinkedinIcon";
+
 export const Footer = () => {
-  return <div className="p-2 border-t border-gray-500">hi there</div>;
+  return (
+    <div className="container mx-auto flex justify-between p-4 items-center border-gray-500">
+      <p className="text-gray-400 mb-4 md:mb-0">
+        © 2024 Your Company. All rights reserved.
+      </p>
+      <div className="flex gap-2">
+        <GithubIcon />
+        <LinkedinIcon />
+      </div>
+    </div>
+  );
 };
