3f3fd60f8c218c389ec0d30a9048512bab38a793
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-12-24 23:51:09 +0530

    Refactor LandingCard component layout for improved styling and responsiveness

--- client/src/components/LandingCard.tsx
+++ client/src/components/LandingCard.tsx
@@ -8,7 +8,7 @@ interface LandingCardProps {
 
 export const LandingCard = ({ title, text, icon }: LandingCardProps) => {
   return (
-    <div className="relative p-4 mx-auto border border-blue-500 border-opacity-10 rounded-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 max-w-xs">
+    <div className="relative mx-4 p-4 rounded-lg">
       {icon}
       <h3 className="text-lg font-bold my-2">{title}</h3>
       <p className="text-gray-300 text-sm mt-2">{text}</p>
