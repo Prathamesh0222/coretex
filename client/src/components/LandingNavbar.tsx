5a8f3dedf06c0d07de529881b0ab585db946948c
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-12-26 23:35:31 +0530

    Add animation to LandingNavbar for improved user experience

--- client/src/components/LandingNavbar.tsx
+++ client/src/components/LandingNavbar.tsx
@@ -1,9 +1,20 @@
 import { BrainlyIcon } from "../icons/BrainlyIcon";
 import { Button } from "./Button";
+import { motion } from "framer-motion";
 
 export const LandingNavbar = () => {
   return (
-    <div className="container mx-auto p-4 flex items-center text-white justify-between">
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.2,
+        type: "spring",
+        stiffness: 100,
+      }}
+      className="container mx-auto p-4 flex items-center text-white justify-between"
+    >
       <div className="flex gap-2 items-center">
         <BrainlyIcon />
         <h1 className="text-2xl font-bold">Brainly</h1>
@@ -12,6 +23,6 @@ export const LandingNavbar = () => {
         <Button variant="primary" text="Login" />
         <Button variant="border" text="Join Now" />
       </div>
-    </div>
+    </motion.div>
   );
 };
