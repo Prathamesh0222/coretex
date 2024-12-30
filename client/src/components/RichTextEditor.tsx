003a142416ce57a7df30d49ca18bb07b55823294
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2024-12-30 23:48:43 +0530

    Implement Rich Text Editor with customizable toolbar and content management; update styles for improved layout and readability

--- client/src/components/RichTextEditor.tsx
+++ client/src/components/RichTextEditor.tsx
@@ -1,19 +1,135 @@
-import { EditorProvider } from "@tiptap/react";
+import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { Menubar } from "./Menubar";
+import BulletList from "@tiptap/extension-bullet-list";
+import ListItem from "@tiptap/extension-list-item";
+import OrderedList from "@tiptap/extension-ordered-list";
 
-export const RichTextEditor = () => {
-  const extensions = [StarterKit];
+interface RichTextEditorProps {
+  editorContent: string;
+  onChange: (content: string) => void;
+}
 
-  const content = "<p>Hello World!</p>";
+const RichTextEditor = ({ editorContent, onChange }: RichTextEditorProps) => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      ListItem,
+      BulletList.configure({
+        HTMLAttributes: {
+          class: "list-disc ml-2",
+        },
+      }),
+      OrderedList.configure({
+        HTMLAttributes: {
+          class: "list-decimal ml-2",
+        },
+      }),
+    ],
+    content: editorContent,
+    onUpdate: ({ editor }) => {
+      onChange(editor.getHTML());
+    },
+  });
+
+  if (!editor) {
+    return null;
+  }
 
   return (
-    <div>
-      <EditorProvider
-        slotBefore={<Menubar />}
-        extensions={extensions}
-        content={content}
-      ></EditorProvider>
+    <div className="flex flex-col justify-stretch min-h-[200px] border rounded border-b-0">
+      <div className="flex items-center gap-2 mb-2 p-2">
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={`p-2 rounded ${
+            editor.isActive("bold") ? "bg-gray-200" : "text-white"
+          }`}
+          title="Bold (Ctrl+B)"
+        >
+          <b>B</b>
+        </button>
+
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={`p-2 rounded ${
+            editor.isActive("italic") ? "bg-gray-200" : "text-white"
+          }`}
+          title="Italic (Ctrl+I)"
+        >
+          <i>I</i>
+        </button>
+
+        <button
+          type="button"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 1 }).run()
+          }
+          className={`p-2 rounded ${
+            editor.isActive("heading", { level: 1 })
+              ? "bg-gray-200"
+              : "text-white"
+          }`}
+          title="Heading 1"
+        >
+          H1
+        </button>
+
+        <button
+          type="button"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={`p-2 rounded ${
+            editor.isActive("heading", { level: 2 })
+              ? "bg-gray-200"
+              : "text-white"
+          }`}
+          title="Heading 2"
+        >
+          H2
+        </button>
+
+        <button
+          type="button"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className={`p-2 rounded ${
+            editor.isActive("heading", { level: 3 })
+              ? "bg-gray-200"
+              : "text-white"
+          }`}
+          title="Heading 3"
+        >
+          H3
+        </button>
+
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={`p-2 rounded ${
+            editor.isActive("bulletList") ? "bg-gray-200" : "text-white"
+          }`}
+          title="Bullet List"
+        >
+          B
+        </button>
+
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={`p-2 rounded ${
+            editor.isActive("orderedList") ? "bg-gray-200" : "text-white"
+          }`}
+          title="Ordered List"
+        >
+          ol
+        </button>
+      </div>
+      <EditorContent editor={editor} />
     </div>
   );
 };
+
+export default RichTextEditor;
