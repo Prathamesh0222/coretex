19b84cf57170008db6224b3eb16acefa780b7059
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2025-01-01 05:39:16 +0530

    Remove CORS middleware and set Access-Control-Allow-Origin header directly

--- http/src/index.ts
+++ http/src/index.ts
@@ -2,13 +2,10 @@ import express from "express";
 import userRouter from "./Router/userRouter";
 import contentRouter from "./Router/contentRouter";
 import { config } from "dotenv";
-import cors from "cors";
 
 config();
 const app = express();
 app.use(express.json());
-
-app.use(cors());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
