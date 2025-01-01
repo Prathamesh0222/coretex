b358661773e4c7eb297b754c7c6772f0d9cb1a5b
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2025-01-01 05:46:10 +0530

    Add CORS middleware with specific origin and method configurations

--- http/src/index.ts
+++ http/src/index.ts
@@ -2,10 +2,17 @@ import express from "express";
 import userRouter from "./Router/userRouter";
 import contentRouter from "./Router/contentRouter";
 import { config } from "dotenv";
+import cors from "cors";
 
 config();
 const app = express();
 app.use(express.json());
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://second-brain-alpha.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -15,6 +22,8 @@ app.use(function (req, res, next) {
   );
   next();
 });
+
+app.use(cors(corsOptions));
 
 app.use("/api/v1", userRouter);
 app.use("/api/v1", contentRouter);
