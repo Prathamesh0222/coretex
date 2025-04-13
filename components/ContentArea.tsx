9c538c024aa0e96856988b4e0a892eb8a8ef72b6
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-04-13 22:47:38 +0530

    implement tag input functionality and YouTube preview in ContentArea component; add Badge component for tag display

--- components/ContentArea.tsx
+++ components/ContentArea.tsx
@@ -18,8 +18,32 @@ import {
 } from "@/components/ui/sheet";
 import { Input } from "./ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { KeyboardEvent, useState } from "react";
+import { toast } from "sonner";
+import { Badge } from "./ui/badge";
 
 export const ContentArea = () => {
+  const [isPreview, setIsPreview] = useState<string>("");
+  const [tagsInput, setTagsInput] = useState<string>("");
+  const [tags, setTags] = useState<string[]>([]);
+
+  const handleTagInput = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const newTag = tagsInput.trim();
+      if (tags.includes(newTag)) {
+        toast.error("Duplicate tags are not allowed");
+      } else {
+        setTags([...tags, newTag]);
+        setTagsInput("");
+      }
+    }
+  };
+
+  const handleRemoveTagInput = (tagId: string) => {
+    setTags(tags.filter((tag) => tag !== tagId));
+  };
+
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-between">
@@ -41,26 +65,55 @@ export const ContentArea = () => {
               </SheetDescription>
             </SheetHeader>
             <div className="mx-4">
-              <Tabs defaultValue="account">
+              <Tabs defaultValue="content">
                 <TabsList>
-                  <TabsTrigger value="account">Account</TabsTrigger>
-                  <TabsTrigger value="password">Password</TabsTrigger>
+                  <TabsTrigger value="content">Content</TabsTrigger>
+                  <TabsTrigger value="Notes">Notes</TabsTrigger>
                 </TabsList>
-                <TabsContent value="account">
+                <TabsContent value="content">
                   <div className="space-y-4">
                     <label className="text-sm font-semibold">Title</label>
                     <Input />
-                    <label className="text-sm font-semibold">Title</label>
+                    <label className="text-sm font-semibold">
+                      Content Type
+                    </label>
                     <Input />
-                    <label className="text-sm font-semibold">Title</label>
-                    <Input />
-                    <label className="text-sm font-semibold">Title</label>
-                    <Input />
+                    <label className="text-sm font-semibold">Link</label>
+                    <Input onChange={(e) => setIsPreview(e.target.value)} />
+                    {isPreview && (
+                      <div className="border p-4 rounded-xl">
+                        <iframe
+                          width="320"
+                          height="250"
+                          src={isPreview
+                            .replace("watch", "embed")
+                            .replace("?v=", "/")}
+                          title="YouTube video player"
+                          frameBorder="0"
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                          referrerPolicy="strict-origin-when-cross-origin"
+                          allowFullScreen
+                          className="rounded-xl"
+                        ></iframe>
+                      </div>
+                    )}
+
+                    <label className="text-sm font-semibold">Tags</label>
+                    <Input
+                      type="text"
+                      placeholder="Press enter to add the tag"
+                      value={tagsInput}
+                      onChange={(e) => setTagsInput(e.target.value)}
+                      onKeyDown={handleTagInput}
+                    />
+                    <div className="flex gap-2">
+                      {tags.map((tag) => (
+                        <Badge className="rounded-lg">{tag}</Badge>
+                      ))}
+                    </div>
                   </div>
                 </TabsContent>
-                <TabsContent value="password">
-                  Change your password here.
-                </TabsContent>
+                <TabsContent value="Notes">Change your Notes here.</TabsContent>
               </Tabs>
             </div>
           </SheetContent>
