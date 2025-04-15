d3930e04689dde674d69b34a9025c3b15efe6d04
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-04-15 16:18:19 +0530

    add SpotifyTab component to embed Spotify album; enhance user experience with interactive media integration

---
+++ components/SpotifyTab.tsx
@@ -0,0 +1,14 @@
+export const SpotifyTab = () => {
+  return (
+    <iframe
+      style={{ borderRadius: "12px" }}
+      src="https://open.spotify.com/embed/album/5KF4xCxDD8ip003hoatFT9?utm_source=generator&theme=0"
+      width="100%"
+      height="352"
+      frameBorder="0"
+      allowFullScreen
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
+  );
+};
