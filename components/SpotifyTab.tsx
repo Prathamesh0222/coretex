9cf9db476b81cafb50e6b9491db8fc6d1e3830cb
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-04-15 22:24:41 +0530

    refactor SpotifyTab component to allow dynamic embedding of Spotify links; implement input validation and preview functionality for improved user experience

--- components/SpotifyTab.tsx
+++ components/SpotifyTab.tsx
@@ -1,14 +1,50 @@
+import { useContentState } from "@/app/store/contentState";
+import { Input } from "./ui/input";
+import { toast } from "sonner";
+
 export const SpotifyTab = () => {
+  const { isPreview, setIsPreview } = useContentState();
+
+  const getEmbedUrl = (link: string) => {
+    try {
+      const url = new URL(link);
+      const pathParts = url.pathname.split("/");
+      const type = pathParts[1];
+      const id = pathParts[2];
+      return `https://open.spotify.com/embed/${type}/${id}`;
+    } catch (error) {
+      toast.error("Invalid Spotify Link. Please paste a valid Spotify URL.");
+      return null;
+    }
+  };
+
   return (
-    <iframe
-      style={{ borderRadius: "12px" }}
-      src="https://open.spotify.com/embed/album/5KF4xCxDD8ip003hoatFT9?utm_source=generator&theme=0"
-      width="100%"
-      height="352"
-      frameBorder="0"
-      allowFullScreen
-      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-      loading="lazy"
-    ></iframe>
+    <>
+      <label className="text-sm font-semibold">Spotify Link</label>
+      <Input
+        placeholder="Paste Spotify link here..."
+        onChange={(e) => {
+          const link = e.target.value;
+          setIsPreview(link);
+          if (link && !link.includes("open.spotify.com")) {
+            toast.error(
+              "Invalid Spotify link. Please paste a valid Spotify URL."
+            );
+          }
+        }}
+      />
+      {isPreview && getEmbedUrl(isPreview) && (
+        <iframe
+          style={{ borderRadius: "12px" }}
+          src={getEmbedUrl(isPreview) || ""}
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allowFullScreen
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      )}
+    </>
   );
 };
