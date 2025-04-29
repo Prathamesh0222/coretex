ba55eacd4905101ee520628f5cb7981bad57b939
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2025-04-29 14:53:22 +0530

    update Home component to include LandingNavbar, Hero, LandingFeatures, and LandingFooter for a complete landing page layout; enhance structure and user experience with responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import { Hero } from "@/components/Hero";
+import { LandingFeatures } from "@/components/LandingFeatures";
+import { LandingFooter } from "@/components/LandingFooter";
+import { LandingNavbar } from "@/components/LandingNavbar";
+
 export default function Home() {
-  return <div>hi there</div>;
+  return (
+    <div className="max-w-7xl mx-auto h-screen">
+      <LandingNavbar />
+      <Hero />
+      <LandingFeatures />
+      <LandingFooter />
+    </div>
+  );
 }
