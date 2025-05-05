c30229be2ed1b4d3eeac992d3456410337d53b6f
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-05-06 01:07:42 +0530

    Enhance LandingNavbar with routing functionality for Login and Sign up buttons; adjust spacing for better layout consistency

--- components/LandingNavbar.tsx
+++ components/LandingNavbar.tsx
@@ -4,10 +4,12 @@ import { Layers, Moon, Sun } from "lucide-react";
 import { Button } from "./ui/button";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export const LandingNavbar = () => {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState<boolean>(false);
+  const router = useRouter();
 
   useEffect(() => {
     setMounted(true);
@@ -32,19 +34,25 @@ export const LandingNavbar = () => {
           Coretex
         </h1>
       </div>
-      <div className="gap-2 md:space-x-4 items-center flex">
+      <div className="gap-2 md:gap-0 md:space-x-4 items-center flex">
         <Button variant={"ghost"} onClick={() => handleTheme()}>
           {theme === "dark" ? <Sun /> : <Moon />}
         </Button>
         <Button
           variant="outline"
           className="text-md hover:bg-secondary/80 cursor-pointer font-medium"
+          onClick={() => {
+            router.push("/signin");
+          }}
         >
           Login
         </Button>
         <Button
           variant="default"
           className="text-md dark:text-white text-black bg-gradient-to-br from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer font-medium"
+          onClick={() => {
+            router.push("/signup");
+          }}
         >
           Sign up
         </Button>
