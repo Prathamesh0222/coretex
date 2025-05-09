5742ae9cc78378e87d4df3b99dc43ff053e167c0
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-05-09 23:13:02 +0530

    Implement LinkedIn and GitHub click handlers in LandingFooter for external profile navigation; enhance user interaction with social media icons

--- components/LandingFooter.tsx
+++ components/LandingFooter.tsx
@@ -1,12 +1,22 @@
+'use client';
+
 import { Github, Linkedin } from "lucide-react";
 
 export const LandingFooter = () => {
+  const handleLinkedinClick = () => {
+    window.open('https://www.linkedin.com/in/prathamesh-pimpalkar-903b0621a/', '_blank');
+  }
+
+  const handleGithubClick = () => {
+    window.open('https://github.com/Prathamesh0222', '_blank');
+  }
+
   return (
     <div className="container mx-auto p-3 flex justify-between text-muted-foreground pb-8">
       <h1>Developed & Designed By Prathamesh</h1>
       <span className="flex gap-4">
-        <Github className="cursor-pointer hover:scale-115 hover:-rotate-12" />
-        <Linkedin className="cursor-pointer hover:scale-115 hover:-rotate-12" />
+        <Github onClick={handleGithubClick} className="cursor-pointer hover:scale-115 hover:-rotate-12" />
+        <Linkedin onClick={handleLinkedinClick} className="cursor-pointer hover:scale-115 hover:-rotate-12" />
       </span>
     </div>
   );
