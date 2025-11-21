bc76ef2af66767aba9c12d82a98800a3fc4611df
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-11-21 08:26:36 +0530

    Add husky for pre-commit linting and update dependencies in package.json and bun.lock

--- app/providers/CustomProvider.tsx
+++ app/providers/CustomProvider.tsx
@@ -3,7 +3,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 export const CustomProvider = ({ children }: { children: React.ReactNode }) => {
   return (
